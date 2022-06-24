b78b05f066b48b2da53e0d8215623d993ace1913
prb01 <p.bergstroem@gmail.com>
2022-06-24 18:54:36 -0500

    updated logo styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ const Home = (props) => {
   return (
     <div className="w-100 h-100 vh-100 d-flex justify-content-center align-items-center homepage-bg">
       <div>
-        <p className="text-secondary logo-large">SpotList</p>
+        <p className="text-text logo-large">SpotList</p>
         <div className="d-flex justify-content-center"></div>
       </div>
     </div>
