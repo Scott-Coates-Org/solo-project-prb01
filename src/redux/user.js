b0f3df923bc2d628402f8d9f3182d6204f2b2844
prb01 <p.bergstroem@gmail.com>
2022-06-24 11:59:07 -0500

    removed spotify reducer functions from user reducer

--- src/redux/user.js
+++ src/redux/user.js
@@ -22,24 +22,9 @@ const user = createSlice({
       state.isLoaded = true;
       state.hasErrors = true;
     },
-    appendData: (state, action) => {
-      state.isLoaded = true;
-      state.data = {...state.data, ...action.payload};
-    },
   },
 });
 
 export const reducer = user.reducer;
 
-export const { getData, getDataSuccess, getDataFailure, appendData } = user.actions;
-
-export const createSpotifyAuth = createAsyncThunk(
-  "user/createSpotifyAuth",
-  async (payload, thunkAPI) => {
-    try {
-      thunkAPI.dispatch(appendData(payload));
-    } catch (error) {
-      console.log(error)
-    }
-  }
-);
+export const { getData, getDataSuccess, getDataFailure } = user.actions;
