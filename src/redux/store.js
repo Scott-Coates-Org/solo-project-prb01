62877bf003aff0da7a0eb8b11da1082ddec36412
prb01 <p.bergstroem@gmail.com>
2022-06-24 11:59:54 -0500

    added spotify reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import { reducer as user } from './user';
 import { reducer as widget } from './widget';
+import { reducer as spotify } from "./spotify"
 
 const reducer = combineReducers({
     user,
     widget,
+    spotify
 });
 
 const store = configureStore({
