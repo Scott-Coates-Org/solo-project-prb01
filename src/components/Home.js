b746d0271d78f7054ddcd1f081426376726f9106
prb01 <p.bergstroem@gmail.com>
2022-06-24 19:02:17 -0500

    added accent to logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,9 @@ const Home = (props) => {
   return (
     <div className="w-100 h-100 vh-100 d-flex justify-content-center align-items-center homepage-bg">
       <div>
-        <p className="text-text logo-large">SpotList</p>
+        <p className="text-text logo-large">
+          Spot<span className="text-secondary">List</span>
+        </p>
         <div className="d-flex justify-content-center"></div>
       </div>
     </div>
