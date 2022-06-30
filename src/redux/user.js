22ebf493bd3e44e524c7e269d2a6c63afcc9f855
prb01 <p.bergstroem@gmail.com>
2022-06-30 09:57:04 -0500

    expanded user reducer actions to create collection in firebase

--- src/redux/user.js
+++ src/redux/user.js
@@ -5,6 +5,7 @@ const initialState = {
   data: {},
   isLoaded: false,
   hasErrors: false,
+  existsInDb: false,
   errorMsg: {},
 };
 
@@ -24,9 +25,71 @@ const user = createSlice({
       state.hasErrors = true;
       state.errorMsg = action.payload;
     },
+    createDataFailure: (state, action) => {
+      state.hasErrors = true;
+      state.errorMsg = action.payload;
+    },
+    appendData: (state) => {
+      state.isLoaded = false;
+      state.hasErrors = false;
+      state.errorMsg = {};
+    },
+    appendDataSuccess: (state, action) => {
+      state.isLoaded = true;
+      state.data = { ...state.data, ...action.payload };
+    },
+    appendDataFailure: (state, action) => {
+      state.isLoaded = true;
+      state.hasErrors = true;
+      state.errorMsg = action.payload;
+    },
   },
 });
 
 export const reducer = user.reducer;
 
 export const { getData, getDataSuccess, getDataFailure } = user.actions;
+
+export const fetchUser = createAsyncThunk(
+  "user/fetchUser", 
+  async (payload, thunkAPI) => {
+  thunkAPI.dispatch(appendData());
+
+  try {
+    const data = await _fetchUserFromDb(payload.uid);
+    thunkAPI.dispatch(appendDataSuccess(data));
+  } catch (error) {
+    thunkAPI.dispatch(appendDataFailure(error));
+  }
+});
+
+export const createUserData = createAsyncThunk(
+  "user/createUserData",
+  async (payload, thunkAPI) => {
+    try {
+      await _createUserData(payload.uid);
+    } catch (error) {
+      thunkAPI.dispatch(createDataFailure(error));
+    }
+  }
+);
+
+async function _fetchUserFromDb(uid) {
+  const snapshot = await firebaseClient
+    .firestore()
+    .collection("users")
+    .where("uid", "==", uid);
+
+  const data = snapshot.docs.map((doc) => ({ ...doc.data() }));
+
+  return data;
+}
+
+async function _createUserData(uid) {
+  const doc = await firebaseClient
+    .firestore()
+    .collection("users")
+    .add({ uid, access_token: null, refresh_token: null });
+
+  return doc;
+}
