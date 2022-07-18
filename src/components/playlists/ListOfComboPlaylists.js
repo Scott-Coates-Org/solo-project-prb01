49832488137180982327a36fe32d44920da3d19d
prb01 <p.bergstroem@gmail.com>
2022-07-18 13:41:41 +0200

    added title

--- src/components/playlists/ListOfComboPlaylists.js
+++ src/components/playlists/ListOfComboPlaylists.js
@@ -17,6 +17,7 @@ const ListOfComboPlaylists = ({ combinedPlaylists }) => {
       {spotifyHasErrors && "Error Loading"}
       {spotifyIsLoaded && (
         <Container className="p-3 my-3 border border-accent form-rounded text-text d-grid gap-3">
+          <h2 className="text-center mb-4">{spotifyData.user.id}'s Combined Playlists</h2>
           {combinedPlaylists.map((combinedPlaylist) => (
             <ComboPlaylist
               key={combinedPlaylist.id}
