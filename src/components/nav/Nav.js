48b24742ae6ca998c081d26992ecc87d86b4eeb9
prb01 <p.bergstroem@gmail.com>
2022-07-18 14:35:24 +0200

    added Connect Spotify to Nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,14 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import { useSelector } from "react-redux";
 import { Nav as StrapNav, NavItem, NavLink, Button } from "reactstrap";
-import { adminRefreshAllCombinedPlaylists } from "utils/utils";
+import { adminRefreshAllCombinedPlaylists, spotifyLogin } from "utils/utils";
 import logoSmall from "../../assets/img/spotlist.svg";
 
 const Nav = () => {
   const userData = useSelector(state => state.user.data)
+
   // admin function to synch all combined playlists
   const handleRefreshCombinedPlaylists = async () => {
     await adminRefreshAllCombinedPlaylists();
   };
+
+  //(re)Connect to Spotify (auth)
+  const handleConnectSpotify = () => {
+    spotifyLogin()
+  }
 
   return (
     <header className="fixed-top d-flex justify-content-center align-items-center p-2 px-3">
@@ -24,12 +32,26 @@ const Nav = () => {
 
         <StrapNav pills>
           {userData.admin && (
-            <NavItem className="mx-2">
-              <Button color="secondary" className="btn-rounded" onClick={handleRefreshCombinedPlaylists}>
+            <NavItem className="me-2">
+              <Button
+                color="secondary"
+                className="btn-rounded"
+                onClick={handleRefreshCombinedPlaylists}
+              >
                 Refresh All Playlists
               </Button>
             </NavItem>
           )}
+          <NavItem className="me-2">
+            <Button
+              color="secondary"
+              className="btn-rounded d-flex gap-2 align-items-center"
+              onClick={handleConnectSpotify}
+            >
+              <FontAwesomeIcon icon={faSpotify} />
+              (re)Connect Spotify
+            </Button>
+          </NavItem>
           <NavItem className="bg-accent btn-rounded">
             <NavLink href="/logout">
               <b>Logout</b>
