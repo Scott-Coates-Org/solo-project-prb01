9bb06d0271d0ead472c36950b7b0741c88019429
prb01 <p.bergstroem@gmail.com>
2022-08-01 10:56:50 +0200

    refactor idx & name to be on same line for mobile too

--- src/components/playlists/Playlist.js
+++ src/components/playlists/Playlist.js
@@ -3,8 +3,8 @@ import { Col, Row } from "reactstrap";
 const Playlist = ({ playlist, idx }) => {
   return (
     <Row className="text-primary">
-      <Col sm={1}>{idx + 1}</Col>
-      <Col sm={11}>{playlist.name}</Col>
+      <Col xs={1}>{idx + 1}</Col>
+      <Col xs={11}>{playlist.name}</Col>
     </Row>
   );
 };
