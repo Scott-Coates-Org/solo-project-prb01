18f35ae3e0dedd584cfa4524ed62aa895d5737b1
prb01 <p.bergstroem@gmail.com>
2022-08-03 11:01:14 +0200

    added getAccessToken to cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,71 @@
+const axios = require("axios").default;
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+const spotifyState = process.env.REACT_APP_SPOTIFY_STATE;
+const baseURI = "https://accounts.spotify.com";
+const apiURI = "https://api.spotify.com/v1";
+
+// common error to check that user is logged in to run function
+const checkUserLoggedIn = (context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+};
+
+exports.getAccessToken = functions.https.onCall(async (data, context) => {
+  console.log({ data, context });
+  const { code, state, redirectURI } = data;
+
+  // checkUserLoggedIn(context);
+
+  if (state !== spotifyState) {
+    throw new functions.https.HttpsError("invalid-argument", "States are not the same");
+  }
+
+  const formBody = new URLSearchParams();
+  formBody.set("grant_type", "authorization_code");
+  formBody.set("code", code);
+  formBody.set("redirect_uri", redirectURI);
+
+  // const response = await fetch(`${baseURI}/api/token`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     Authorization: "Basic " + new Buffer(clientId + ":" + clientSecret).toString("base64"),
+  //   },
+  //   body: formBody,
+  // });
+
+  try {
+    const response = await axios({
+      url: `${baseURI}/api/token`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " + new Buffer.from(clientId + ":" + clientSecret).toString("base64"),
+      },
+      data: formBody,
+    });
+
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a non 2.x.x status
+      throw new functions.https.HttpsError("unknown", error.message);
+    } else if (error.request) {
+      // The request was made but no response was received
+      throw new functions.https.HttpsError("unavailable", "No response received.");
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+  }
+});
